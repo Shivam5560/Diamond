ba7c9492ff3fecdda270681b46e4c0eec8dcc264
Shivam Sourav <shivamsourav@Shivams-MacBook-Air.local>
2023-07-30 23:06:45 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
+
+cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
+color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
+clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
+
+st.title('PREDICT QUALITY OF FRESH WATER')
+col1, col2, col3 ,col4= st.columns(4)
+with col1:
+    carat=st.number_input("CARAT VALUE",step=1.,format="%.5f")
+    depth = st.number_input("DEPTH VALUE",step=1.,format="%.5f")
+    table = st.number_input("TABLE VALUE",step=1.,format="%.5f")
+with col2:
+    x = st.number_input("X VALUE",step=1.,format="%.5f")
+    y = st.number_input("Y VALUE",step=1.,format="%.5f")
+    z = st.number_input("Z Value",step=1.,format="%.5f")
+with col3:
+    cut = st.selectbox("CUT CATEGORIES",cut_categories)
+    color= st.selectbox("COLOR CATEGORIES",color_categories)
+    clarity = st.selectbox("CLARITY CATEGORIES",clarity_categories)
+
+data=CustomData(carat,depth,table,x,y,z,cut,color,clarity)
+
+def predict():
+    final_new_data=data.get_data_as_dataframe()
+    predict_pipeline=PredictPipeline()
+    pred=predict_pipeline.predict(final_new_data)
+    results=round(pred[0],2)
+    finall = 'Diamond Price = $'+str(results)
+    st.markdown(f'<h1 style="color:#33ff33;font-size:24px;">{finall}</h1>', unsafe_allow_html=True)
+
+button = st.button("PREDICT",on_click=predict)
--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -4,6 +4,8 @@ from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
 from src.components.data_ingestion import DataIngestion
+from src.components.datatransformation import DataTransformation
+from src.components.modeltrainer import ModelTrainer
 
 
 if __name__=='__main__':
@@ -11,3 +13,7 @@ if __name__=='__main__':
     train_data_path,test_data_path=obj.initiate_data_ingestion()
     print(train_data_path,test_data_path)
 
+    data_transformation = DataTransformation()
+    train_arr,test_arr,obj1=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    model_trainer = ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
